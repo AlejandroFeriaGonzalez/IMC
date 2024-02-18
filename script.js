6e8513fe162dda1d9b3d7174b482e386a2de7f58
alejandro20019 <aferiag@unal.edu.co>
2024-02-18 11:30:34 -0500

    👍

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-document.getElementById('imcForm').addEventListener('submit', function(event) {
-  event.preventDefault();
+document.getElementById('imcForm').addEventListener('submit', function (event) {
+  event.preventDefault()
 
-  let weight = document.getElementById('weight').value;
-  let height = document.getElementById('height').value;
-  let historial = document.getElementById('historial');
+  let weight = document.getElementById('peso').value
+  let height = document.getElementById('altura').value
+  let historial = document.getElementById('historial')
 
   // Validar que los valores ingresados sean números válidos
   if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-    document.getElementById('result').textContent = 'Por favor ingresa valores válidos para el peso y la altura.';
-    return; // Detener la ejecución si los datos son inválidos
+    document.getElementById('result').textContent =
+      'Por favor ingresa valores válidos para el peso y la altura.'
+    return // Detener la ejecución si los datos son inválidos
   }
 
-  let imc = weight / (height * height);
+  let imc = weight / (height * height)
 
-  document.getElementById('result').textContent = 'Tu IMC es ' + imc.toFixed(2);
+  let result = document.getElementById('result')
+  result.textContent = 'Tu IMC es ' + imc.toFixed(2)
+
+  if (imc < 18.5) {
+    result.textContent += ' (Bajo peso)'
+  } else if (imc < 24.9) {
+    result.textContent += ' (Peso normal)'
+  } else if (imc < 29.9) {
+    result.textContent += ' (Sobrepeso)'
+  } else {
+    result.textContent += ' (Obesidad)'
+  }
 
   // Agregar botón para guardar los resultados
-  let saveButton = document.createElement('button');
-  saveButton.textContent = 'Guardar resultado';
-  saveButton.addEventListener('click', function() {
+  let saveButton = document.createElement('button')
+  saveButton.textContent = 'Guardar resultado'
+
+  saveButton.addEventListener('click', function () {
     // Lógica para guardar los resultados
-    let savedResult = 'Tu IMC es ' + imc.toFixed(2);
+    let savedResult = 'Tu IMC es ' + imc.toFixed(2)
     // Aquí puedes implementar la lógica para guardar el resultado en una base de datos, archivo, o cualquier otro medio de almacenamiento
-    console.log('Resultado guardado:', savedResult);
-  });
+    console.log('Resultado guardado:', savedResult)
+  })
 
-  historial.innerHTML += '<p>' + 'Tu IMC es ' + imc.toFixed(2) + '</p>';
+  historial.innerHTML += '<p>' + result.textContent + '</p>'
 
-  document.getElementById('result').appendChild(saveButton);
-});
+  document.getElementById('result').appendChild(saveButton)
+})
